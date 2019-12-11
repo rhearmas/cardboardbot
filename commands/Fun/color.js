3fb1334b406b78550c5f40e549b0febab9f987e1
rhearmas <34490428+qu-ota@users.noreply.github.com>
2019-12-11 13:44:59 -0500

    some other random tests

--- commands/Fun/color.js
+++ commands/Fun/color.js
@@ -1,18 +1,22 @@
 exports.run = async (client, message, args, level) => {
-	const response = await client.awaitReply(message, "Favourite Color?");
-	message.reply(`Oh, I really love ${response} too!`);
+	const response = await client.awaitReply(message, "Hey, what's your favorite color?");
+	if(response !== "black") {
+		message.reply(`Oh, I really love ${response} too!`);
+	} else {
+		message.reply(`you like ${response}? Disgusting.`);
+	}
 }
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: "User"
 };
 
 exports.help = {
-  name: "color",
-  category: "Fun",
-  description: "Who likes a color?",
-  usage: "color"
+	name: "color",
+	category: "Fun",
+	description: "Who likes a color?",
+	usage: "color"
 };
