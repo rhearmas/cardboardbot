0afb9ad817d36ba1eb9e56de42741c02b2d4b292
rhearmas <34490428+qu-ota@users.noreply.github.com>
2019-12-11 13:45:24 -0500

    finally fixed shutdown! can now choose whether or not to shutdown just in case it was a mistake

--- commands/System/shutdown.js
+++ commands/System/shutdown.js
@@ -1,14 +1,27 @@
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const response = await client.awaitReply(message, "**Are you sure you want to shut me down?** Respond with \`yes\` to proceed.")
-  const msg = question;
-	console.log(`Response is ${response}`);
-  if(response = "yes") {
-    await msg.edit(`Confirmed by ${message.author} Shutting down...`);
+exports.run = async (client, message, args, level) => {
+  message.delete();
+
+  let response = await client.awaitReply(message, "**Are you sure you want to shut me down?** Respond with \`yes\` to proceed.");
+  message.author.lastMessage.delete();
+  let msg = client.user.lastMessage;
+	
+  if(response === "yes") {
+    await msg.edit(`**Confirmed by ${message.author}.** Shutting down...`);
     await Promise.all(client.commands.map(cmd =>
       client.unloadCommand(cmd)
     ));
-    await msg.edit("Successfully shut down.");
+    await msg.edit(`Successfully shut down by ${message.author}.`);
     process.exit(0);
+  } else if(response === "no") {
+    await msg.edit(`**Shutdown cancelled by ${message.author}.** This message will be removed in 5 seconds.`).then(msg => {
+      msg.delete(5000)
+    });
+    return;
+  } else {
+    await msg.edit(`**${message.author} has provided an invalid response**; shutdown aborted. This message will be deleted in 5 seconds.`).then(msg => {
+      msg.delete(5000)
+    });
+    return;
   }
 };
 
