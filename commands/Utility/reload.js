d4696b15a302076f7a655389fc8abe19c3fd6cb8
rhearmas <34490428+qu-ota@users.noreply.github.com>
2019-12-11 10:46:14 -0500

    beginning to fix reload command so it loads commands in a specific directory

--- commands/Utility/reload.js
+++ commands/Utility/reload.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args, level) => {
   let response = await client.unloadCommand(args[0]);
   if (response) return message.reply(`Error Unloading: ${response}`);
 
-  response = client.loadCommand(command.help.name);
+  response = client.loadCommand(`${command.help.name}`);
   if (response) return message.reply(`Error Loading: ${response}`);
 
   message.reply(`command \`${command.help.name}\` has been reloaded successfully.`);
@@ -20,6 +20,6 @@ exports.conf = {
 exports.help = {
   name: "reload",
   category: "System",
-  description: "Reloads a command that\"s been modified.",
+  description: "Reloads a command that\'s been modified.",
   usage: "reload [command]"
 };
