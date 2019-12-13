f8d5d2efd25f18b7900d585edf4b11e572cdd517
rhearmas <34490428+qu-ota@users.noreply.github.com>
2019-12-13 13:55:54 -0500

    finally it work

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -1,57 +1,61 @@
 /*
 exports.run = async (bot, msg) => {
-    const user = msg.mentions.users.first();
-    if (!user) {
-        throw 'Please mention the user who you want the avatar from.';
-    }
+		const user = msg.mentions.users.first();
+		if (!user) {
+				throw 'Please mention the user who you want the avatar from.';
+		}
 
-    if (!user.avatarURL) {
-        throw 'That user does not have an avatar.';
-    }
+		if (!user.avatarURL) {
+				throw 'That user does not have an avatar.';
+		}
 
-    msg.delete();
-    (await msg.channel.send({
-        embed: bot.utils.embed(`${user.username}'s Avatar`, `[Download](${user.avatarURL})`, [], { image: user.avatarURL })
-    })).delete(30000);
+		msg.delete();
+		(await msg.channel.send({
+				embed: bot.utils.embed(`${user.username}'s Avatar`, `[Download](${user.avatarURL})`, [], { image: user.avatarURL })
+		})).delete(30000);
 };
 
 exports.info = {
-    name: 'avatar',
-    usage: 'avatar <user>',
-    description: 'Gives you the avatar of a user'
+		name: 'avatar',
+		usage: 'avatar <user>',
+		description: 'Gives you the avatar of a user'
 };
 */
 
 exports.run = async (client, message, args, level) => {
-  message.delete();
-  const user = message.mentions.users.first();
-  
-  if (!user) {
-    await message.reply("please mention the user you want the avatar from.").delete(5000);
-    return;
-  }
-  if (!user.avatarURL) {
-    await message.reply("that user does not have an avatar.").delete(5000);
-    return;
-  }
-  
-  await message.channel.send({embed: new Discord.RichEmbed()
-    .setTitle(`${user.username}'s Avatar`)
-    .setImage({image: user.avatarURL})
-    .setFooter(`[Download](${user.avatarURL})`,"https://telegram-stickers.github.io/public/stickers/windows-10/33.png")
-  });
+	let user;
+
+	if (!args[0] || !message.mentions.users.first()) {
+		user = message.author;
+		author = message.author
+		message.delete();
+	} else {
+		user = message.mentions.users.first();
+		author = message.author;
+		message.delete();
+	}
+	if (!user.avatarURL) {
+		await message.delete();
+		await message.reply("that user does not have an avatar.");
+		await client.user.lastMessage.delete(5000);
+		return;
+	}
+	
+	(await message.channel.send(
+		client.embed(user === message.author ? `Your Avatar` : `${user.username}'s Avatar`, `**Sent by ${author}** | [Download](${user.avatarURL}) | :timer: Deleting in 30 seconds`, [], { image: user.avatarURL })
+		)).delete(30000)
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: "User"
 };
 
 exports.help = {
-  name: "avatar",
-  category: "Utility",
-  description: "Gives you the avatar of a user.",
-  usage: "avatar <user>"
+	name: "avatar",
+	category: "Utility",
+	description: "Gives you the avatar of a user.",
+	usage: "avatar <user>"
 };
