9088813f54ad9d9de37eadf48a10a68cd6c4da51
rhearmas <34490428+qu-ota@users.noreply.github.com>
2019-12-21 15:55:14 -0500

    modify status based on what the bot is doing

--- commands/System/shutdown.js
+++ commands/System/shutdown.js
@@ -1,5 +1,5 @@
-exports.run = async (client, message, args, level) => {
   message.delete();
+  client.user.setStatus("idle");
 
   let response = await client.awaitReply(message, "**Are you sure you want to shut me down?** Respond with \`yes\` to proceed.");
   message.author.lastMessage.delete();
@@ -7,20 +7,29 @@ exports.run = async (client, message, args, level) => {
 	
   if(response === "yes") {
     await msg.edit(`**Confirmed by ${message.author}.** Shutting down...`);
+
+    client.user.setStatus("dnd");
+
     await Promise.all(client.commands.map(cmd =>
       client.unloadCommand(cmd)
     ));
     await msg.edit(`Successfully shut down by ${message.author}.`);
+    
+    client.user.setStatus("invisible");
     process.exit(0);
   } else if(response === "no") {
     await msg.edit(`**Shutdown cancelled by ${message.author}.** This message will be removed in 5 seconds.`).then(msg => {
       msg.delete(5000)
     });
+
+    client.user.setStatus("online");
     return;
   } else {
     await msg.edit(`**${message.author} has provided an invalid response**; shutdown aborted. This message will be deleted in 5 seconds.`).then(msg => {
       msg.delete(5000)
     });
+
+    client.user.setStatus("online");
     return;
   }
 };
