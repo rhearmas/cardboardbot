75be73d63c43a60d0031905147b073e3664fddcb
rhearmas <34490428+qu-ota@users.noreply.github.com>
2019-12-23 20:21:37 -0500

    i did a lot and i'm too lazy to comment each commit lol

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,5 +1,3 @@
-// This event executes when a new guild (server) is left.
-
 module.exports = (client, guild) => {
   client.logger.cmd(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`);
   
