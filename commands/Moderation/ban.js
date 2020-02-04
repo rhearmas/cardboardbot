d34c9f4f4f594132c6d7eb3cc1c3d0a0007c52e0
carol <34490428+rhearmas@users.noreply.github.com>
2020-02-04 11:15:16 -0500

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,19 +1,21 @@
 exports.run = async (client, message, args, level) => {
-  const user = message.mentions.users.first() || client.users.get("username", args[0]).id;
-  parseUser(message, user);
-  const modlog = client.channels.find('name', config.modLogChannel);
-  const caseNum = await caseNumber(client, modlog);
-  if (!modlog) return message.reply('I cannot find a valid modlog channel.');
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
-  // message.guild.ban(user, 2);
+	const user = message.mentions.users.first() || client.users.get("username", args[0]).id;
+	parseUser(message, user);
+	
+	const modlog = client.channels.find('name', config.modLogChannel);
+	const caseNum = await caseNumber(client, modlog);
+	
+	if (!modlog) return message.reply('I cannot find a valid modlog channel.');
+	if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
+	// message.guild.ban(user, 2);
 
-  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
-  const embed = new RichEmbed()
-  	.setColor(0x00AE86)
-  	.setTimestamp()
-  	.setDescription(`**Action:** Ban\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-  	.setFooter(`Case ${caseNum}`);
-  return client.channels.get(modlog.id).send({embed});
+	const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
+	const embed = new RichEmbed()
+		.setColor(0x00AE86)
+		.setTimestamp()
+		.setDescription(`**Action:** Ban\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
+		.setFooter(`Case ${caseNum}`);
+	return client.channels.get(modlog.id).send({embed});
 };
 
 exports.conf = {
