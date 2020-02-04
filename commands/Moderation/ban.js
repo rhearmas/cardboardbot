b5453e9563af9584d98699160b84de6b88c6766a
carol <34490428+rhearmas@users.noreply.github.com>
2020-02-04 11:12:23 -0500

    Create ban.js

---
+++ commands/Moderation/ban.js
@@ -0,0 +1,31 @@
+exports.run = async (client, message, args, level) => {
+  const user = message.mentions.users.first();
+  parseUser(message, user);
+  const modlog = client.channels.find('name', config.modLogChannel);
+  const caseNum = await caseNumber(client, modlog);
+  if (!modlog) return message.reply('I cannot find a valid modlog channel.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
+  // message.guild.ban(user, 2);
+
+  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
+  const embed = new RichEmbed()
+  	.setColor(0x00AE86)
+  	.setTimestamp()
+  	.setDescription(`**Action:** Ban\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
+  	.setFooter(`Case ${caseNum}`);
+  return client.channels.get(modlog.id).send({embed});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "ban",
+  category: "Moderation",
+  description: "You've been hit by Banny Billy!",
+  usage: "ban <user> <reason>"
+};
