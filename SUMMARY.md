d5a001bddf3c44d6ce77c5df93e11528083a6a63
Bliss Knieriem <me@rhearmas.net>
2021-05-31 18:51:55 -0400

    Implement new code into master

--- SUMMARY.md
+++
@@ -1,8 +0,0 @@
-# Table of contents
-
-* [Welcome](README.md)
-
-## Bot Usage
-
-* [Host the bot yourself](bot-usage/host-the-bot-yourself.md)
-
--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = async client => {
-  client.logger.log(`${client.user.tag}, ready to serve ${client.users.filter(user => !user.bot).size} users in ${client.guilds.size} ${client.guilds.size > 1 ? "servers" : "server"}.`, "ready");
-
-  client.user.setActivity(`${client.users.filter(user => !user.bot).size} humans | ${client.settings.get("default").prefix}help`, {type: "WATCHING"});
-  client.user.setStatus("online");
-};
